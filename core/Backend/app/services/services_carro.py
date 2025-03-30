1e44b45a48b6c0507eb3e13589e57649d7199561
Kelven <kelvenr40@gmail.com>
2025-03-30 16:43:17 -0300

    fix documentation in services cars

--- core/Backend/app/services/services_carro.py
+++ core/Backend/app/services/services_carro.py
@@ -26,6 +26,32 @@ class ServiceCarros:
         Tipo, Quilometragem, Cor, Portas, Lugares,
         Combustivel, Descricao, Endereco, Imagem
         ):
+        """
+        Cria um novo carro com as informações fornecidas e armazena mongodb
+
+        Args:
+            Marca (str): A marca do carro.
+            Modelo (str): O modelo do carro.
+            Ano (int): O ano de fabricação do carro.
+            Preco (float): O preço do carro.
+            Disponivel (bool): Indica se o carro está disponível para venda.
+            Tipo (str): O tipo do carro.
+            Quilometragem (float): A quilometragem do carro.
+            Cor (str): A cor do carro.
+            Portas (int): O número de portas do carro.
+            Lugares (int): O número de lugares disponíveis no carro.
+            Combustivel (str): O tipo de combustível utilizado pelo carro.
+            Descricao (str): Uma descrição detalhada do carro.
+            Endereco (str): O endereço onde o carro está localizado.
+            Imagem (UploadFile): O arquivo de imagem do carro.
+
+        Returns:
+            CaminhaoInfo: Um objeto contendo as informações do carro criado.
+
+        Raises:
+            Exception: Para outros erros que possam ocorrer durante a criação do carro.
+        """
+
         # Salva a imagem no diretório uploads
         file_location = os.path.join(UPLOAD_DIRECTORY, Imagem.filename)
         with open(file_location, "wb") as file_object:
@@ -59,12 +85,22 @@ class ServiceCarros:
             msg=f"Carro inserido! "
         )
 
+
         # Converte para o modelo CarroInfo, incluindo o id
         return CarroInfo.from_mongo(carro_db)
 
 
     @staticmethod
     async def getCarsService():
+        """
+        Realiza um get par retornar todos os carros no banco de dados
+        Args:
+            nenhum dados como parametros
+        Returns:
+            um objeto mongodb é transformando em um objeto pytdantic e retornado em forma de lista
+        Raises:
+            Caso nao exista nenhum carro no banco de dados, not fount 404
+        """
 
         carros_cursor = db.carros.find()
         carros = [CarroInfo.from_mongo(carro) for carro in await carros_cursor.to_list(length=100)]
@@ -85,6 +121,15 @@ class ServiceCarros:
     @staticmethod
     # Função para converter second_search para o tipo adequado
     def convert_search_value(value: str, campo: str):
+        """
+        Args:
+            value recebe um numero inteiro relacionado ao valor do campo
+            campo recebe o valor e verifica se é possivel consultar com o campo passado
+        Returns:
+            retorna a permissao para a realizacao da consulta
+        Raises:
+            Caso nao seja encontrado campo ou value, erro 400, bad request
+        """
         try:
             # Tentando converter conforme o tipo do campo
             if campo in ["ano", "preco", "quilometragem", "portas", "lugares"]:
@@ -96,6 +141,18 @@ class ServiceCarros:
     
     @staticmethod
     async def getCarWithParamsService(first_params, second_params):
+
+        """
+        Args:
+            recebe dois tipos de parametros para realizar consulta
+            first_params: sendo string
+            second_params: sendo inteiro
+        Returns:
+            o resultado referente com os parametros de pesquisa 
+        Raises:
+            caso first_params nao esteja setado em campos_validos, retorna 400, bad request
+            caso nao possua carros: 404, not found
+        """
         # Validar se o campo é permitido
         campos_validos = [
             "marca", "modelo", "ano",
@@ -140,6 +197,14 @@ class ServiceCarros:
     
     @staticmethod
     async def getCarByIdService(carro_id):
+        """
+        Args:
+            carro_id sera passado como um objeto 'json', já que o id é um uuid
+        Return:
+            caso o id esteja no banco de dados, o objeto mongo db é transformando para pydantic, e o valor retornado
+        Raises:
+            caso o carro nao exista: 404, not found
+        """
         try:
             # Tenta converter o carro_id para ObjectId, porque o MongoDB trabalha com objetos!
             carro_object_id = ObjectId(carro_id)
@@ -170,6 +235,14 @@ class ServiceCarros:
 
     @staticmethod
     async def getCarPageService(request):
+        """
+        Args:
+            request é passao para relaizar a consulta no banco de dados e renderizar no front
+        Returns:
+            os dados retornados nao tratados e renderizados
+        Raises:
+            caso nenhum carro exista: 404, not found
+        """
         carros_cursor = db.carros.find()
         carros = [CarroInfo.from_mongo(carro) for carro in await carros_cursor.to_list(length=100)]
 
@@ -186,6 +259,32 @@ class ServiceCarros:
         Tipo, Quilometragem, Cor, Portas, Lugares,
         Combustivel, Descricao, Endereco, Imagem
         ):
+        """
+        atualiza as informacoes do veiculo
+        Args:
+            Marca (str): A marca do carro.
+            Modelo (str): O modelo do carro.
+            Ano (int): O ano de fabricação do carro.
+            Preco (float): O preço do carro.
+            Disponivel (bool): Indica se o carro está disponível para venda.
+            Tipo (str): O tipo do carro.
+            Quilometragem (float): A quilometragem do carro.
+            Cor (str): A cor do carro.
+            Portas (int): O número de portas do carro.
+            Lugares (int): O número de lugares disponíveis no carro.
+            Combustivel (str): O tipo de combustível utilizado pelo carro.
+            Descricao (str): Uma descrição detalhada do carro.
+            Endereco (str): O endereço onde o carro está localizado.
+            Imagem (UploadFile): O arquivo de imagem do carro.
+        
+        Return:
+            retorna as informacoes do veiculo ja atualizado
+        
+        Raises:
+            caso o id seja invalido: 400, bad request
+            caso o carro nao exista: 404, not found
+
+        """
         try:
             # Tenta converter o carro_id para ObjectId, porque o MongoDB trabalha com objetos!
             carro_object_id = ObjectId(carro_id)
@@ -243,6 +342,15 @@ class ServiceCarros:
 
     @staticmethod
     async def deleteCarByIdService(carro_id):
+        """
+        Args:
+            carro_id é passado para realilzar consulta no banco de dados
+        Returns:
+            ao deletar veiculo, é retornado status code 204, not content
+        Raises:
+            caso o id seja invalido: 400, bad request
+            caso o carro nao exista: 404, not found
+        """
         try:
             # Tenta converter o carro_id para ObjectId, porque o MongoDB trabalha com objetos!
             carro_object_id = ObjectId(carro_id)
