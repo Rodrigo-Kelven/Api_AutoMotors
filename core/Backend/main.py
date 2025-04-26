0630bff0a8fe0a91d8534f38cb7270cb3d5e5357
Kelven <kelvenr40@gmail.com>
2025-04-26 14:11:53 -0300

    REFACT, initialization of process

--- core/Backend/main.py
+++ core/Backend/main.py
@@ -17,36 +17,23 @@ app = FastAPI(
     version="2.1.10"
 )
 
+# Monta o diretório de uploads para servir imagens
+# esta parte é crucial, se não for montado aqui, as imagens nao irão renderizar
+# pois o fastapi vai entender que o diretorio não foi montado
+app_logger.info(msg="Pasta para o armazenamento das imagens criadas.")
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+app.add_middleware(LogRequestMiddleware)
+
 # chama todas as rotas para o app FastAPI antes de qualquer processamento
 all_routes(app)
 
 @app.on_event("startup")
 async def startup_event():
     try:
-
-        # Monta o diretório de uploads para servir imagens
-        # esta parte é crucial, se não for montado aqui, as imagens nao irão renderizar
-        # pois o fastapi vai entender que o diretorio não foi montado
-        app_logger.info(msg="Pasta para o armazenamento das imagens criadas.")
-        app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-        app.add_middleware(LogRequestMiddleware)
-
-
         # Criação das tabelas no banco de dados de usuários
         async with engine_auth.begin() as conn:
             await conn.run_sync(Base_auth.metadata.create_all)
             db_logger.info("Tabela UserDB criada com sucesso.")
-
-        
-        
-        # Adiciona o middleware ao FastAPI, verifica requests e responses
-        app.add_middleware(LogRequestMiddleware)
-
-        # funcao para configuracao do middleware
-        app.middleware("http")(rate_limit_middleware)
-
-        # adicionar CORS para implementacao com o frontend
-        config_CORS(app)
 
     except Exception as e:
         db_logger.error(f"Erro ao criar tabelas: {str(e)}.")
@@ -56,3 +43,13 @@ async def startup_event():
 async def shutdown_event():
     await engine_auth.dispose()
     db_logger.info("Conexões com os bancos de dados encerradas.")
+
+
+# Adiciona o middleware ao FastAPI, verifica requests e responses
+app.add_middleware(LogRequestMiddleware)
+
+# funcao para configuracao do middleware
+app.middleware("http")(rate_limit_middleware)
+
+# adicionar CORS para implementacao com o frontend
+config_CORS(app)
